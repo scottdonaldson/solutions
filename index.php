09485f0bfb60a3b3d6efd667f0d6b62b8c345b59
Scott Donaldson <scott.p.donaldson@gmail.com>
2013-02-04 13:39:44 -0600

    icon fonts and news page

--- index.php
+++ index.php
@@ -6,20 +6,32 @@
 
 	<section <?php post_class('news'); ?>>
 		<h3><?php the_title(); ?></h3>
-		<?php 
-		$images = get_field('featured_images');
-		if ($images) {
-			$count = count($images);
-			if ($count == 1) {
-				while (has_sub_field('featured_images')) {
-					the_sub_field('image');
+		<div class="meta">
+			<div class="date sans"><?php $date = get_the_date('n.j.y'); echo $date; ?></div>
+			<div class="share">
+				<a href="" class="icon-facebook"></a>
+				<a href="" class="icon-twitter"></a><br>
+				<a href="" class="icon-linkedin"></a>
+				<a href="" class="icon-google-plus"></a><br>
+				<a class="border blacklink hoverable link" href="<?php the_permalink(); ?>">article link</a>
+			</div>
+		</div>
+		<div class="content">
+			<?php 
+			$images = get_field('featured_images');
+			if ($images) {
+				$count = count($images);
+				if ($count == 1) {
+					while (has_sub_field('featured_images')) {
+						the_sub_field('image');
+					}
+				} elseif ($count == 2) {
+					while (has_sub_field('featured_images')) {
+						
+					}
 				}
-			} elseif ($count == 2) {
-				while (has_sub_field('featured_images')) {
-					
-				}
-			}
-		} ?>
+			} ?>
+		</div>
 	</section>
 
 <?php endwhile; endif; ?>
--- js/plugins.js
+++ js/plugins.js
@@ -10,3 +10,33 @@ if (!(window.console && console.log)) {
         }
     }());
 }
+
+// IcoMoon for IE8 and below
+if ($('html').hasClass('lt-ie8')) {
+	window.onload = function() {
+		function addIcon(el, entity) {
+			var html = el.innerHTML;
+			el.innerHTML = '<span style="font-family: \'icomoon\'">' + entity + '</span>' + html;
+		}
+		var icons = {
+				'icon-facebook' : '&#xe000;',
+				'icon-twitter' : '&#xe001;',
+				'icon-linkedin' : '&#xe002;',
+				'icon-google-plus' : '&#xe003;'
+			},
+			els = document.getElementsByTagName('*'),
+			i, attr, html, c, el;
+		for (i = 0; i < els.length; i += 1) {
+			el = els[i];
+			attr = el.getAttribute('data-icon');
+			if (attr) {
+				addIcon(el, attr);
+			}
+			c = el.className;
+			c = c.match(/icon-[^\s'"]+/);
+			if (c && icons[c[0]]) {
+				addIcon(el, icons[c[0]]);
+			}
+		}
+	};
+}
