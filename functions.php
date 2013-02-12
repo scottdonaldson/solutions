af2f7b27869c58c734b8cbdfd49b5bdd7ad5acbe
Scott Donaldson <scott.p.donaldson@gmail.com>
2013-02-11 20:29:01 -0600

    downgraded to jQuery 1.8.3... latest compatible with WordPress

--- functions.php
+++ functions.php
@@ -7,8 +7,7 @@ register_nav_menu('primary', 'Primary Menu');
 if (!is_admin()) add_action("wp_enqueue_scripts", "my_jquery_enqueue", 11);
 function my_jquery_enqueue() {
     wp_deregister_script('jquery');
-    // As of Jan. 2013, latest jQuery is 1.9.0
-    wp_register_script('jquery', "http" . ($_SERVER['SERVER_PORT'] == 443 ? "s" : "") . "://ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js", false, null);
+    wp_register_script('jquery', "http" . ($_SERVER['SERVER_PORT'] == 443 ? "s" : "") . "://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js", false, null);
     wp_enqueue_script('jquery');
 }
 
