b9ad2b1f2bfe9442288c27ddec662a4c8aec2020
Scott Donaldson <scott.p.donaldson@gmail.com>
2013-02-13 15:03:08 -0600

    moving process indicator (w/ background animate)

--- pages/process.php
+++ pages/process.php
@@ -11,7 +11,7 @@ the_post(); ?>
 	<div class="step step-1">
 		<div class="bar"></div>
 		<div class="line"></div>
-		<h3 class="round"><div class="sol-hoverable">1</div></h3>
+		<h3><span>1</span></h3>
 		<section>
 			<h4 class="header">Project Initiation</h4>
 			<p>Proposal, feasibility assessment customer purchase order</p>
@@ -20,7 +20,7 @@ the_post(); ?>
 	<div class="step step-2">
 		<div class="bar"></div>
 		<div class="line"></div>
-		<h3 class="round"><div class="sol-hoverable">2</div></h3>
+		<h3><span>2</span></h3>
 		<section>
 			<h4 class="header">Planning Phase</h4>
 			<p>Develop plan and design inputs (requirements)</p>
@@ -29,13 +29,13 @@ the_post(); ?>
 	<div class="step design">
 		<div class="bar"></div>
 		<div class="line"></div>
-		<h3 class="round"><div class="sol-hoverable icon-pencil"></div></h3>
+		<h3><span class="icon-pencil"></span></h3>
 		<section><h4 class="header">Design Review</h4></section>
 	</div>
 	<div class="step step-3">
 		<div class="bar"></div>
 		<div class="line"></div>
-		<h3 class="round"><div class="sol-hoverable">3</div></h3>
+		<h3><span>3</span></h3>
 		<section>
 			<h4 class="header">Design Phase</h4>
 			<p>System, electronic, software and mechanical design descriptions</p>
@@ -44,13 +44,13 @@ the_post(); ?>
 	<div class="step design">
 		<div class="bar"></div>
 		<div class="line"></div>
-		<h3 class="round"><div class="sol-hoverable icon-pencil"></div></h3>
+		<h3><span class="icon-pencil"></span></h3>
 		<section><h4 class="header">Design Review</h4></section>
 	</div>
 	<div class="step step-4">
 		<div class="bar"></div>
 		<div class="line"></div>
-		<h3 class="round"><div class="sol-hoverable">4</div></h3>
+		<h3><span>4</span></h3>
 		<section>
 			<h4 class="header">Implementation Phase</h4>
 			<p>Schematics, drawings, software code, manufacturing procedures, part specifications, vendor selection, etc.</p>
@@ -59,13 +59,13 @@ the_post(); ?>
 	<div class="step design">
 		<div class="bar"></div>
 		<div class="line"></div>
-		<h3 class="round"><div class="sol-hoverable icon-pencil"></div></h3>
+		<h3><span class="icon-pencil"></span></h3>
 		<section><h4 class="header">Design Review</h4></section>
 	</div>
 	<div class="step step-5">
 		<div class="bar"></div>
 		<div class="line"></div>
-		<h3 class="round"><div class="sol-hoverable">5</div></h3>
+		<h3><span>5</span></h3>
 		<section>
 			<h4 class="header">Verification Phase</h4>
 			<p>Electronic, software, mechanical and integration testing</p>
@@ -74,24 +74,24 @@ the_post(); ?>
 	<div class="step design">
 		<div class="bar"></div>
 		<div class="line"></div>
-		<h3 class="round"><div class="sol-hoverable icon-pencil"></div></h3>
+		<h3><span class="icon-pencil"></span></h3>
 		<section><h4 class="header">Design Review</h4></section>
 	</div>
 	<div class="step step-6">
 		<div class="bar"></div>
 		<div class="line"></div>
-		<h3 class="round"><div class="sol-hoverable">6</div></h3>
+		<h3><span>6</span></h3>
 		<section><h4 class="header">Validation</h4></section>
 	</div>
 	<div class="step design">
 		<div class="bar"></div>
 		<div class="line"></div>
-		<h3 class="round"><div class="sol-hoverable icon-pencil"></div></h3>
+		<h3><span class="icon-pencil"></span></h3>
 		<section><h4 class="header">Design Review</h4></section>
 	</div>
 	<div class="step step-7 final">
 		<div class="bar"></div>
-		<h3 class="round"><div class="sol-hoverable">7</div></h3>
+		<h3><span>7</span></h3>
 		<section>
 			<h4 class="header">Release to Production</h4>
 			<p>Design transfer to customer</p>
@@ -112,19 +112,28 @@ jQuery(document).ready(function($){
 	var barWidth = bar.first().width();
 	bar.not(':first').width(0);
 	steps.not(':eq(0)').find('h3, section').hide();
-	steps.first().addClass('active shown');
+	steps.first().addClass('active shown').find('h3').prepend('<div class="indicator"></div>');
 
 	var nextStep = function(current){
-		if (!isShowing && !current.hasClass('final')) {
+		if (!isShowing) {
 			isShowing = true;
-			next = current.next('.step') ? current.next('.step') : false;
+			next = current.next('.step');
 			
-			if (next) {
+			if (next.length > 0) {
 				next.addClass('shown');
 				current.removeClass('active').find('.line').animate({
 					'height': '100%'
 				}, 200, function(){
 					next.find('h3').fadeIn(200, function(){
+						current.find('.indicator').animate({
+							'top': current.height() - 60
+						}, 200, 'linear', function(){
+							current.find('.indicator').prependTo(next.find('h3')).css({
+								'top': -60
+							}).animate({
+								'top': -10
+							}, 100, 'linear');
+						});
 						next.addClass('active').find('.bar').stop().animate({
 							'margin-left': 0,
 							'width': barWidth
@@ -136,6 +145,10 @@ jQuery(document).ready(function($){
 						});
 					});
 				});
+			} else {
+				$('.indicator').animate({
+					'backgroundColor': 'rgba(64,136,199,0.5)'
+				}, 800);
 			}
 		}
 	};
@@ -143,18 +156,11 @@ jQuery(document).ready(function($){
 		nextStep(steps.first());
 	}, 1000);
 
-	$(document).click(function(){
-		var last = $('.shown').last();
-		nextStep(last);
-	});
-
 	var step;
 	$(window).scroll(function(){
 		// If the mid-point of our window is below the last shown step,
 		// then show the next step
 		step = $('.shown:last');
-		// console.log('step: ' + step.offset().top);
-		// console.log('window: ' + ($(window).scrollTop() + $(window).height()/2));
 		if ($(window).scrollTop() + $(window).height()/2 > step.offset().top) {
 			nextStep(step);
 		}
