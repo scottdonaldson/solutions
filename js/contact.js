c3a402dbeb49d3cfd46edb8d2078fc88455e16eb
Scott Donaldson <scott.p.donaldson@gmail.com>
2013-03-02 16:32:59 -0600

    multiple team members

---
+++ js/contact.js
@@ -0,0 +1,94 @@
+jQuery(document).ready(function($){
+
+	// "Get in touch!"
+	$('h2.huge a').click(function(e){
+		e.preventDefault();
+
+		$('html, body').animate({
+			'scrollTop': $('#contact').offset().top
+		}, 1200);
+	})
+
+	// The map - spotlight follows mouse
+	var map = $('.map'),
+		container = map.find('.container'),
+		x, y;
+
+	$('.map').mousemove(function(e){
+		$this = $(this);
+		x = e.pageX - $this.offset().left;
+		y = e.pageY - $this.offset().top;
+		container.css({
+			'left': x,
+			'top': y
+		});
+	}).mouseleave(function(){
+		container.animate({
+			'left': '62%',
+			'top': '28%'
+		}, 500);
+	}); 
+
+	// Team members
+	var fulls = $('#fulls'),
+		products = $('.product').not(':first'),
+		samples = $('.sample'),
+		sample,
+		target, targetHeight,
+		n;
+
+	products.find('.left, h3, p').hide();
+
+	products.append('<div class="close">'+
+					    '<div class="bg-hover"></div>'+
+					    '<div class="icon-close"></div>'+
+					    '<div class="border-clone"></div>'+
+					'</div>');
+	$('.product').first().find('p:last-child').addClass('last');
+	products.find('.close').prev('p').addClass('last');
+
+	$('.close').click(function(){
+		$this = $(this);
+		target = $this.closest('.product');
+		target.fadeOut({queue: false}).slideUp(800, function(){
+			target.find('.left, h3, p').hide();
+			target.addClass('gone');
+		});
+
+		n = target.attr('data-n');
+		samples.filter(function(index){ // find the corresponding sample and show it again
+			return $(this).attr('data-n') === n;
+		}).animate({
+			'margin-right': '2%',
+			'opacity': 1
+		}, function(){
+			$(this).removeAttr('style');
+		});
+
+		// in case there were 0 visible samples left, this resets the height of container
+		$this.closest('#featured').removeAttr('style');
+	});
+
+	samples.click(function(){
+		$this = $(this);
+		// Fade out the image
+		$this.animate({
+			'opacity': 0,
+			'margin-right': '-100%'
+		}, 300, function(){
+			$this.hide();
+		});
+
+		// Time to show the featured product
+		target = products.eq($this.index());
+		setTimeout(function(){
+			target.appendTo(fulls).find('.left, h3, p').fadeIn();
+			target.slideDown().animate({
+				'height': '100%'
+			}, function(){
+				target.removeClass('gone').fadeIn();
+			});
+		}, 300);
+
+	});
+});
--- pages/contact.php
+++ pages/contact.php
@@ -3,7 +3,13 @@
 Template Name: Contact
 */
 get_header(); 
-the_post(); ?>
+the_post(); 
+
+function helper_js() { ?>
+	<script src="<?php echo bloginfo('template_url'); ?>/js/contact.js"></script>
+<?php } 
+add_action('wp_footer', 'helper_js');
+?>
 
 <h1 class="visuallyhidden"><?php the_title(); ?></h1>
 
@@ -28,62 +34,54 @@ the_post(); ?>
 	<p class="uppercase"><a href="http://maps.google.com/maps?daddr=287+E+6th+St+%23140,+St+Paul,+MN+55101&hl=en&sll=44.951045,-93.086236&sspn=0.009643,0.022724&geocode=FQXmrQId5J1z-g&mra=ls&t=m&z=16" target="_blank">Get Google directions.</a></p>
 </div>
 
-<section <?php post_class('team'); ?>>
-	<?php 
-	if (count(get_field('team')) == 1) { 
-		echo '<h2 class="header">Team Leader</h2>';
-	} elseif (count(get_field('team')) > 1) {
-		echo '<h2 class="header">The Team</h2>';
-	} 
-	if (get_field('team')) {
-		$t = 0;
-		while (has_sub_field('team')) { ?>
-			<div class="member <?php if ($t == 0) { echo 'first'; } ?>">
-				<img src="<?php the_sub_field('photo'); ?>" alt="<?php the_sub_field('name'); ?>">
-				
-				<div class="right">
-					<h3><?php the_sub_field('name'); if (get_sub_field('title')) { echo ' - '.get_sub_field('title'); } ?></h3>
-					<a href="mailto:<?php the_sub_field('email'); ?>"><?php the_sub_field('email'); ?></a>
-					<p><?php the_sub_field('bio'); ?></p>
-				</div>
-			</div>
+<?php 
+if (get_field('team')) { 
+	$p = 0; ?>
+	<section id="featured" <?php post_class('clearfix team'); ?>>
 		<?php 
-		$t++;
-		}
-	} ?>
+		if (count(get_field('team')) == 1) { 
+			echo '<h2 class="header">Team Leader</h2>';
+		} elseif (count(get_field('team')) > 1) {
+			echo '<h2 class="header">The Team</h2>';
+		} ?>
+		<div class="ie7 border-clone"></div>
+		
+		<div id="fulls">
+			<?php 
+			// Full images with titles and descriptions
+			while (has_sub_field('team')) { ?>
+				<div data-n="<?php echo $p; ?>" class="product clearfix <?php if ($p > 0) { echo 'gone'; } ?>">
 
-	<div class="ie7 border-clone"></div>
-</section>
+					<div class="left">
+						<img src="<?php the_sub_field('photo'); ?>" alt="<?php the_sub_field('name'); ?>">
+					</div>
+					<div class="right">
+						<h3><?php the_sub_field('name'); if (get_sub_field('title')) { echo ' - '.get_sub_field('title'); } ?></h3>
+						<a href="mailto:<?php the_sub_field('email'); ?>"><?php the_sub_field('email'); ?></a>
+						<p><?php the_sub_field('bio'); ?></p>
+					</div>
+				</div>
+				<?php $p++;
+			} 
+			// Reset for sample images
+			$p = 0; ?>
+		</div>
 
-<script>
-	// "Get in touch!"
-	$('h2.huge a').click(function(e){
-		e.preventDefault();
-
-		$('html, body').animate({
-			'scrollTop': $('#contact').offset().top
-		}, 1200);
-	})
-
-	var map = $('.map'),
-		container = map.find('.container'),
-		x, y;
-
-	$('.map').mousemove(function(e){
-		$this = $(this);
-		x = e.pageX - $this.offset().left;
-		y = e.pageY - $this.offset().top;
-		container.css({
-			'left': x,
-			'top': y
-		});
-	}).mouseleave(function(){
-		container.animate({
-			'left': '62%',
-			'top': '28%'
-		}, 500);
-	}); 
-
-</script>
+		<div id="samples" class="clearfix">
+		<?php while (has_sub_field('team')) {
+			if ($p > 0) { 
+				$images = get_sub_field('photo'); ?>
+				<div data-n="<?php echo $p; ?>" class="sample sample-<?php echo $p; ?>">
+					<div>
+						<img src="<?php the_sub_field('photo'); ?>" alt="<?php the_sub_field('name'); ?>">
+						<span class="icon-plus"></span>
+					</div>
+				</div>
+		<?php } 
+		$p++;
+		} ?>
+		</div>
+	</section>
+<?php } ?>
 
 <?php get_footer(); ?>
